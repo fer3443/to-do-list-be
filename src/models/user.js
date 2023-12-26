6abf04db5c62650d2a08a48de6f348142db5acb8
Fernando <fer3443@gmail.com>
2023-12-26 12:05:40 -0300

    userScheme, userController y userRoutes agregados

---
+++ src/models/user.js
@@ -0,0 +1,27 @@
+import mongoose, { Schema } from "mongoose";
+
+const userScheme = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  userName: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  avatar: String,
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
+
+export default mongoose.model('user', userScheme);
